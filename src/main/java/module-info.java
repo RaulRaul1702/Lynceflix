d5fc4d4755ac4d522c46c4daa4801d17eaea3014
alexeigio <escuelavgvnnlx@gmail.com>
2023-11-28 09:11:40 -0600

    Se agrega el Proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.lynceflix {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.lynceflix to javafx.fxml;
+    exports com.example.lynceflix;
+}
